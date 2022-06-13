4ac1b41962bcfe1b6a53113af0108676704fabc7
Josue <josue@pihuala.com.ar>
2022-06-13 16:41:06 -0300

    entrega 1

--- mail.js
+++ mail.js
@@ -1,15 +1,46 @@
 class Usuario{
     constructor (nombre, apellido){
-        this.nombre = nombre
-        this.apellido = apellido
-        this.libros = []
-        this.mascotas = []
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._libros = [];
+        this._mascotas = [];
     }
     getFullName(){
-        return `${this.nombre} ${this.apellido}`
+        return `${this._nombre} ${this._apellido}`;
     }
-    addMascota(){
-        
+    addMascota(string){
+        this._mascotas.push(string);
+    }
+    countMascotas(){
+        return this._mascotas.length;
+    }
+    addBook(nombreLibro, autorLibro){
+        let libroAgregar ={
+            nombre: nombreLibro,
+            autor: autorLibro
+        }
+
+        this._libros.push(libroAgregar)
+    }
+    getBookNames(){
+        return this._libros.map((object) => {
+            return object.nombre
+        })
     }
 
+
 }
+
+let usuario = new Usuario ('Josue', 'Pihuala')
+
+usuario.addMascota('Byron')
+usuario.addMascota('Ringo')
+
+usuario.addBook('El Señor de las moscas', 'William Golding')
+usuario.addBook('Fundacion', 'Issav Asimov')
+
+console.log(usuario)
+console.log(usuario.countMascotas())
+console.log(usuario.getBookNames())
+console.log(usuario.getFullName())
+
