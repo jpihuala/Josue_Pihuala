aedfd8b566500e38ffd3e3ae993bf716394f9b4b
Josue <josue@pihuala.com.ar>
2022-06-28 20:12:08 -0300

    practica2

--- practicaC2.js
+++ practicaC2.js
@@ -13,3 +13,19 @@ const dogs = {
   const myDog = "Hunter";
   const myBreed = dogs[myDog];
   console.log(myBreed)
+
+
+const mostrarLista2 = () => {
+    console.log(`lista vacia`)
+}
+
+mostrarLista2()
+
+
+const mostrarLista3 = params => {
+    console.log(params)
+}
+
+const ejecutar = unaFuncion => unaFuncion();
+const saludar = () => console.log("saludos")
+ejecutar (saludar)
