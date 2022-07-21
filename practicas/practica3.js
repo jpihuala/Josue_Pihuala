2346785fe24bfdbf6c13b09c9acc5b1da704cfcc
jpihuala <josue@pihuala.com.ar>
2022-07-21 14:03:17 -0300

    ordenar

--- practicas/practica3.js
+++ practicas/practica3.js
@@ -1,24 +1,24 @@
-console.log("hola josue");
+// console.log("hola josue");
 
 
-let obj={}
+// let obj={}
 
-const agregar = (nro) => {
-    if (typeof obj [nro] === 'undefined') {
-        obj [nro] = 1
-    } else{
-        obj [nro] += 1
-    }
-}
+// const agregar = (nro) => {
+//     if (typeof obj [nro] === 'undefined') {
+//         obj [nro] = 1
+//     } else{
+//         obj [nro] += 1
+//     }
+// }
 
-Math.random()
-for (let index = 0; index < 1000; index++) {
-    const element = Math.ceil(Math.randon() * 20);
-    agregar(element)
+// Math.random()
+// for (let index = 0; index < 1000; index++) {
+//     const element = Math.ceil(Math.randon() * 20);
+//     agregar(element)
     
-}
+// }
 
-console.log(obj)
+// console.log(obj)
 
 
 
