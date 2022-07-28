f545481272b02c7833882091b642d49da749ac83
jpihuala <josue@pihuala.com.ar>
2022-07-28 15:29:42 -0300

    entrega a medias

--- entrega3/src/server.js
+++ entrega3/src/server.js
@@ -1,34 +1,19 @@
-const express = require("express")
-
-
+const express = require("express");
+const fs = require("fs")
+const productos = JSON.parse(fs.readFileSync("../productos.txt", "utf-8"));
 const app=express();
 
-app.get('/', (req, res)=>{
-    res.send('<h1 style="color: #3d33ff ">Bienvenidos al servidor express</h1>')
-})
+app.get("/productos", (req, res) => {
+    res.send({productos});
+});
 
+app.get("/productosRandom", (req, res) => {
+    res.send(Math.random({productos}));
+});
 
-let count = 0
-app.get('/visitas', (req, res)=>{
-    count ++
-    res.send(`visitas ${count}`)
-})
-
-app.get('/fyh', (req, res)=>{
-    const fyh = new Date()
-    res.send(`${fyh}`)
-})
-
-const PORT = 4040;
+const PORT = 8080;
 const server = app.listen(PORT,()=>
     console.log(`server http://localhost:${PORT}`)
 );
-
-// const fs = require("fs")
-
-// const productos = JSON.parse(fs.readFileSync("./productos.txt", "utf-8"));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-
 
 server.on("error", err=> console.log (err));
