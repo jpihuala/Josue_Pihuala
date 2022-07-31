e3c63bd9c218c0072ec41f967c85f91cbc3120c9
Josue Pihuala <josue@pihuala.com.ar>
2022-07-31 18:47:45 -0300

    practicas

--- practica5/main.js
+++ practica5/main.js
@@ -4,6 +4,10 @@ const server = http.createServer((req, res)=>{
     res.end('Hola mundo')
 })
 
+const connectedServer = server.listen(8080, ()=>{
+    console.log('server ${connectedServer.addres().port}')
+})
+
 
 // console.log('hola mundo')
 
