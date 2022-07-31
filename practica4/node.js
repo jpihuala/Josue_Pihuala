6f7a8d6801c155aecfbc7743c0ec3b774ecb576b
jpihuala <josue@pihuala.com.ar>
2022-07-31 13:06:09 -0300

    practias nuevas

--- practica4/node.js
+++ practica4/node.js
@@ -1,11 +1,30 @@
 const fs = require('fs');
 
-try {
-    const data = fs.readFileSync('./package.json', 'utf-8')
-    console.log(data)
-} catch (error) {
-    console.log(error);
-}
+fs.readFile('./package.json', 'utf-8', (err, contenido) =>
+{
+    let obj = {
+        contenidoSrt: contenido, //String
+        contenidoObj: JSON.parse(contenido),  //Objeto
+        size: contenido.length
+    }
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(obj)
+        console.log(contenido)        
+    }
+
+    fs.writeFile('./prueba2.txt', JSON.stringify(obj, null, 2), 'utf-8', (err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log('archivo nuevo')
+        }
+    })
+})
+
+
+
 
 // const info = {
 //     contenidoStr: (contenido del archivo leido en string),
